9443a2ab627ded81494c35f1648b10d02fddeef6
Catherine Kim <ikptlsmile@gmail.com>
2022-07-31 14:00:16 +0900

    3.0 REACT

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {BiSearch} from "react-icons/bi"
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import $ from 'jquery';
 
 function App(){
+  return(
+    <article>
+      <h3><BiSearch /></h3>
+    </article>
+  )
+  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
